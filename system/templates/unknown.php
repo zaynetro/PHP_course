f05a68a16eccdeb24b43e6ede63d6127a4671ff8
Roman Zaynetdinov <romanznet@gmail.com>
2014-11-23 23:28:15 +0200

    Add unknown page template

---
+++ system/templates/unknown.php
@@ -0,0 +1,10 @@
+<?php $this->load_template("header"); ?>
+
+<div class='container'>
+
+  <h2>Not found</h2>
+  <div>Nothing to do here buddy</div>
+
+</div>
+
+<?php $this->load_template("footer"); ?>
