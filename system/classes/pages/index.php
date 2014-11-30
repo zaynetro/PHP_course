3fa3dfde48c835202800a7af822962bccb1af073
Roman Zaynetdinov <romanznet@gmail.com>
2014-11-30 16:03:44 +0200

    Design simple homepage

--- system/classes/pages/index.php
+++ system/classes/pages/index.php
@@ -6,6 +6,13 @@
       parent::__construct($pages);
 
       $this->page->title = "Home";
+
+      $this->_index();
+    }
+
+    private function _index() {
+      $this->page->movies = $this->db->query("SELECT movie_id, title FROM MOVIES ORDER BY created_at LIMIT 5");
+      $this->page->actors = $this->db->query("SELECT actor_id, name FROM ACTORS ORDER BY created_at LIMIT 5");
     }
 
   }
--- system/templates/index.php
+++ system/templates/index.php
@@ -1,14 +1,31 @@
 <?php $this->load_template("sections/header"); ?>
-HELLO!
 
-<h3>Page</h3>
-<pre>
-<?php var_dump($this->page); ?>
-</pre>
+<?php
+  $movies = $this->page->movies;
+  $actors = $this->page->actors;
+?>
 
-<h3>User</h3>
-<pre>
-<?php var_dump($this->u); ?>
-</pre>
+<div class='container'>
+
+  <h1>Welcome to our movie database!</h1>
+
+  <h2>Last added movies</h2>
+  <ul>
+    <?php foreach ($movies as $movie) { ?>
+    <li><a href='/movies/<?= $movie['movie_id']; ?>'><?= $movie['title']; ?></a></li>
+    <?php } ?>
+  </ul>
+
+  <h2>Last added actors</h2>
+  <ul>
+    <?php foreach ($actors as $actor) { ?>
+    <li><a href='/actors/<?= $actor['actor_id']; ?>'><?= $actor['name']; ?></a></li>
+    <?php } ?>
+  </ul>
+
+  <h2>Last reviews</h2>
+  <ul></ul>
+
+</div>
 
 <?php $this->load_template("sections/footer"); ?>
